a50ed45538e24f2c5b1e5bf663c4092edb22804e
Nelson Lora <nlorax@gmail.com>
2024-05-20 13:02:38 -0400

    Stable v1

--- index.js
+++ index.js
@@ -35,10 +35,10 @@ const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-  const pathName = req.url;
+  const { query, pathname } = url.parse(req.url, true);
 
   // Overview page
-  if (pathName === "/" || pathName === "/overview") {
+  if (pathname === "/" || pathname === "/overview") {
     res.writeHead(200, {
       "content-type": "text/html",
     });
@@ -51,11 +51,17 @@ const server = http.createServer((req, res) => {
     res.end(output);
   }
   // Product page
-  else if (pathName === "/product") {
-    res.end("This is the PRODUCT");
+  else if (pathname === "/product") {
+    res.writeHead(200, {
+      "content-type": "text/html",
+    });
+
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
   }
   // API
-  else if (pathName === "/api") {
+  else if (pathname === "/api") {
     res.writeHead(200, {
       "content-type": "application/json",
     });
